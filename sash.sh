c7301651f9e01ff89066314e85aadfb1aed58a92
Uri Agassi <uri.agassi@perion.com>
2014-04-24 10:33:18 +0300

    code review

--- sash.sh
+++ sash.sh
@@ -3,18 +3,19 @@
 #
 
 function private_dns_to_name {
-  if [ -z $1 ]; then
+  dns=$1
+  if [ -z $dns ]; then
     echo "Please enter private dns (ip-10-0-0-XX)"
     return 1
   fi
   local instance_id instance_name
-  instance_id=$(aws ec2 describe-instances --filter "Name=private-dns-name,Values=$1.*" --query "Reservations[*].Instances[*].InstanceId" --output text)
+  instance_id=$(aws ec2 describe-instances --filter "Name=private-dns-name,Values=$dns.*" --query "Reservations[*].Instances[*].InstanceId" --output text)
   if [ -z $instance_id ]; then
-    instance_id=$(aws ec2 describe-instances --filter "Name=private-dns-name,Values=$1*" --query "Reservations[*].Instances[*].InstanceId" --output text)
+    instance_id=$(aws ec2 describe-instances --filter "Name=private-dns-name,Values=$dns*" --query "Reservations[*].Instances[*].InstanceId" --output text)
   fi
 
   if [ -z $instance_id ]; then
-    echo "No machine found with private dns $1"
+    echo "No machine found with private dns $dns"
   fi
   
   instance_name=$(aws ec2 describe-tags --filter "Name=key,Values=Name" "Name=resource-id,Values=$instance_id" --query "Tags[].Value" --output text)
@@ -25,7 +26,8 @@ function private_dns_to_name {
 
 # connect to machine
 function sash {
-  if [ -z $1 ]; then
+  host=$1
+  if [ -z $host ]; then
     echo "Please enter machine name"
     return 1
   fi
@@ -37,19 +39,22 @@ function sash {
   fi
   let pem_idx=idx*2-1
   let ip_idx=pem_idx+1
-  instance=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[].[KeyName,PublicIpAddress]' --output text)
+  instance=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$host" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[].[KeyName,PublicIpAddress]' --output text)
 
   if [ -z "${instance}" ]; then
-    instance=$(aws ec2 describe-instances --filters "Name=private-ip-address,Values=$1" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[].[KeyName,PublicIpAddress]' --output text)
+    instance=$(aws ec2 describe-instances --filters "Name=private-ip-address,Values=$host" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[].[KeyName,PublicIpAddress]' --output text)
     if [ -z "${instance}" ]; then
-      echo Could not find an instance named $1
+      echo Could not find an instance named $host
       return 1
     fi
   fi
-  ip=$(echo $instance | awk "{print \$$ip_idx}")
-  pem=$(echo $instance | awk "{print \$$pem_idx}")
+  set -- $(awk "{print \$$ip_idx, \$$pem_idx}" <<< $instance)
+  #ip=$(echo $instance | awk "{print \$$ip_idx}")
+  #pem=$(echo $instance | awk "{print \$$pem_idx}")
+  ip=$1
+  pem=$2
 
-  echo "Connecting to $1 ($ip)"
+  echo "Connecting to $host ($ip)"
   ssh -i ~/.aws/$pem.pem ubuntu@$ip
 }
 
@@ -64,10 +69,8 @@ function _sash {
     fi
 
     local curw
-    COMPREPLY=()
     curw=${COMP_WORDS[COMP_CWORD]}
     COMPREPLY=($(compgen -W "${_sash_instances}" -- $curw))
-    return 0
 }
 
 complete -F _sash sash
